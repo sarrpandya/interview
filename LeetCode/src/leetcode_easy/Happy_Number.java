cfd7f12783a515258b6c9dc9b77cd713ff37f6c4
sarrpandya <pandya.saurin@gmail.com>
2016-07-18 22:36:54 -0700

    happy_n_commit

--- LeetCode/src/leetcode_easy/Happy_Number.java
+++ LeetCode/src/leetcode_easy/Happy_Number.java
@@ -4,10 +4,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Happy_Number {
+	
 	public static boolean isHappy(int n) {
 		Set<Integer> set = new HashSet<Integer>();
 		while (set.add(n)) {
 
+			/*
+			 * logic of adding the sum value and check if last value comp to 1 
+			 */
 			int sum = 0;
 			while (n > 0) {
 				sum += (int) Math.pow(n % 10, 2);
